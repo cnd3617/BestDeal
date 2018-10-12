16c5ebb62328d0ee22aba06110de2425a87509a2
Richard Dally <r.dally@protonmail.com>
2018-10-12 15:37:35 +0200

    Log category changed + remove comments

--- bestdeal.py
+++ bestdeal.py
@@ -35,13 +35,11 @@ class BestDeal:
                 if product_type:
                     self.update_price(product_name, product_type, source.__name__, product_price)
                 else:
-                    self.logger.info('Ignoring [{}]'.format(product_name))
+                    self.logger.debug('Ignoring [{}]'.format(product_name))
 
     def update_price(self, product_name, product_type, source_name, new_price):
         source_id = self.db.insert_if_necessary(table='source',  columns=['source_name'], values=[source_name])
         product_id = self.db.insert_if_necessary(table='product', columns=['product_name', 'product_type'], values=[product_name, product_type])
-        # self.logger.info('Cheapest price ever [{}] for [{}] source [{}]'.format(self.db.get_cheapest_price(product_id, source_id), product_name, source_name))
-        # self.logger.info('Cheapest price [{}] today for [{}] source [{}]'.format(self.db.get_cheapest_price_for_date(product_id, source_id, self.db.today_date), product_name, source_name))
         last_price = self.db.get_last_price(product_id, source_id)
         if last_price is None or last_price != new_price:
             previous_price_info = 'Previous price [{}]'.format(last_price) if last_price else ''
