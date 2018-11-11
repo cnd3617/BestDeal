b13b690ba5c6d03b35d30c80940115108ed90466
Richard Dally <r.dally@protonmail.com>
2018-11-11 13:53:19 +0100

    Work on frontend to trace multiple sources

--- frontend.py
+++ frontend.py
@@ -1,10 +1,10 @@
 # coding: utf-8
 
-import bestdeal
-import plotly
 import dash
 import dash_core_components as dcc
 import dash_html_components as html
+import plotly
+import bestdeal
 
 external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
 
@@ -15,32 +15,31 @@ colors = {
     'text': '#7FDBFF'
 }
 
+product_type = 'GTX 1080'
 title = html.H1(
-        children='Hello Dash',
+        children='{} price history'.format(product_type),
         style={
             'textAlign': 'center',
             'color': colors['text']
         }
     )
 
-description = html.Div(children='Dash: A web application framework for Python.', style={
+description = html.Div(children='Grab the best deals among all vendors easily', style={
         'textAlign': 'center',
         'color': colors['text']
     })
 
 bd = bestdeal.BestDeal()
-
 data = []
-ts = bd.db.get_time_series(source_id=1, product_type='GTX 1080')
-
-for source_id in [1, 2, 3]:
-    ts = bd.db.get_time_series(source_id=source_id, product_type='GTX 1080')
+for source in bd.db.get_source_identifiers():
+    ts = bd.db.get_time_series(source_id=source['source_id'], product_type=product_type)
     x = []
     y = []
     for record in ts:
         x.append(record['histo_date'])
         y.append(record['histo_price'])
-    data.append(plotly.graph_objs.Scatter(x=x, y=y, connectgaps=True))
+    data.append(plotly.graph_objs.Scatter(name=source['source_name'], x=x, y=y))
+    #data.append(plotly.graph_objs.Scatter(name=source['source_name'], x=x, y=y, connectgaps=True))
 
 graph = dcc.Graph(
         id='example-graph-2',
@@ -55,10 +54,6 @@ graph = dcc.Graph(
             }
         }
     )
-
-#trace = plotly.graph_objs.Scatter(x = [10, 20, 30], y = [500, 1000, 1500])
-#data = [trace]
-#plotly.plotly.iplot(data, filename='basic-line')
 
 app.layout = html.Div(style={'backgroundColor': colors['background']}, children=[title, description, graph])
 
