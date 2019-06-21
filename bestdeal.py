135fc56d6ca95aa1ea7a8e4b3fb33f6a0b4a2db1
Richard Dally <r.dally@protonmail.com>
2019-06-21 20:36:37 +0200

    Fit log padding to maximum length field

--- bestdeal.py
+++ bestdeal.py
@@ -8,6 +8,7 @@ from rueducommerce import RueDuCommerce
 from cybertek import Cybertek
 from ldlc import LDLC
 from loguru import logger
+from collections import namedtuple
 
 
 class BestDeal:
@@ -39,10 +40,16 @@ class BestDeal:
         return product_type
 
     def display_best_deals(self):
-        cheapest_products = self.db.get_cheapest_by_product_type()
         logger.info('Best deals for [{}]'.format(self.db.get_today_date()))
+        cheapest_products = self.db.get_cheapest_by_product_type()
+        max_lengths = {}
         for product in cheapest_products:
-            logger.info('Cheapest [{product_type:7}] [{histo_price:7}]€ [{product_name:115}] [{source_name:13}]'.format(**product))
+            for key, value in product.items():
+                max_lengths.setdefault(key, 0)
+                max_lengths[key] = max(max_lengths[key], len(value))
+        for product in cheapest_products:
+            template = 'Cheapest [{product_type:' + str(max_lengths['product_type']) + '}] [{histo_price:' + str(max_lengths['histo_price']) + '}]€ [{product_name:' + str(max_lengths['product_name']) + '}] [{source_name:' + str(max_lengths['source_name']) + '}]'
+            logger.info(template.format(**product))
 
     def scrap_and_store(self):
         sources = [TopAchat(),
@@ -57,12 +64,36 @@ class BestDeal:
             except Exception as exception:
                 logger.warning('Failed to fetch deals for [{}]. Reason [{}]'.format(source.source_name, exception))
                 continue
+            update_price_details = []
             for product_name, product_price in deals.items():
                 product_type = self.extract_product_type(product_name)
                 if product_type:
-                    self.update_price(product_name, product_type, source.source_name, float(product_price))
+                    update_price_detail = self.update_price(product_name, product_type, source.source_name, float(product_price))
+                    if update_price_detail is not None:
+                        update_price_details.append(update_price_detail)
                 else:
                     logger.debug('Ignoring [{}]'.format(product_name))
+            if update_price_details:
+                self.format_log_update_price_details(update_price_details)
+
+    def format_log_update_price_details(self, update_price_details):
+        """
+        Bloat code to fit to maximum length
+        """
+        product_name_max_length = 0
+        source_name_max_length = 0
+        new_price_max_length = 0
+        today_last_price_max_length = 0
+        for detail in update_price_details:
+            product_name_max_length = max(product_name_max_length, len(detail.product_name))
+            source_name_max_length = max(source_name_max_length, len(detail.source_name))
+            new_price_max_length = max(new_price_max_length, len(detail.new_price))
+            if detail.today_last_price is not None:
+                today_last_price_max_length = max(today_last_price_max_length, len(detail.today_last_price))
+        template = 'New price for [{:' + str(product_name_max_length) + '}] from [{:' + str(source_name_max_length) + '}] : [{:' + str(new_price_max_length) + '}]{}'
+        for detail in update_price_details:
+            previous_price_info = ' Today last price [{:' + str(today_last_price_max_length) + '}]'.format(detail.today_last_price) if detail.today_last_price else ''
+            logger.info(template.format(detail.product_name, detail.source_name, detail.new_price, previous_price_info))
 
     def update_price(self, product_name, product_type, source_name, new_price):
         source_id = self.db.insert_if_necessary(table='source',
@@ -72,10 +103,12 @@ class BestDeal:
                                                  columns=['product_name', 'product_type'],
                                                  values=[product_name, product_type])
         today_last_price = self.db.get_last_price_for_today(product_id, source_id)
+        update_price_details = None
+        UpdatePriceDetails = namedtuple('UpdatePriceDetails', 'product_name source_name new_price today_last_price')
         if today_last_price is None or today_last_price != new_price:
-            previous_price_info = 'Today last price [{:7}]'.format(today_last_price) if today_last_price else ''
-            logger.info('New price for [{:115}] from [{:13}] : [{:7}] {}'.format(product_name, source_name, new_price, previous_price_info))
             self.db.add_price(product_id, source_id, new_price, self.db.get_today_datetime())
+            update_price_details = UpdatePriceDetails(product_name, source_name, str(new_price), str(today_last_price))
+        return update_price_details
 
 
 if __name__ == '__main__':
