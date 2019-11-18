d9418821b0029e8dd5e7b20b7048e7bda24502c9
Richard Dally <r.dally@protonmail.com>
2019-11-18 20:26:38 +0100

    Add 4 unit tests

--- bestdeal_test.py
+++ bestdeal_test.py
@@ -4,12 +4,35 @@ import unittest
 from bestdeal import BestDeal
 
 
-class TestBestDeal(unittest.TestCase):
-    def setUp(self):
-        pass
-
-    def test_find_exactly_one_element(self):
+class TestFindExactlyOneElement(unittest.TestCase):
+    def test_case_1(self):
         brands = ["GAINWARD", "ASUS"]
         product_data = "6GB Gainward GeForce GTX 1660 Pegasus OC Aktiv PCIe 3.0 x16 (Retail)"
         result = BestDeal.find_exactly_one_element(pattern_data=brands, raw_data=product_data)
         self.assertIsNotNone(result, "GAINWARD brand should be found")
+
+
+class TestExtractProductData(unittest.TestCase):
+    def test_case_1(self):
+        product_description = 'Palit GeForce GTX 1660 Ti StormX, 6 Go'
+        brand, product_type = BestDeal.extract_product_data(product_description)
+        self.assertEqual('PALIT', brand)
+        self.assertEqual('1660 TI', product_type)
+
+    def test_case_2(self):
+        product_description = 'Zotac Gaming GeForce GTX 1060 AMP Edition, 6 Go + jeu offert ! '
+        brand, product_type = BestDeal.extract_product_data(product_description)
+        self.assertEqual('ZOTAC', brand)
+        self.assertEqual('1060', product_type)
+
+    def test_case_3(self):
+        product_description = 'MSI 2060 SuPeR'
+        brand, product_type = BestDeal.extract_product_data(product_description)
+        self.assertEqual('MSI', brand)
+        self.assertEqual('2060 SUPER', product_type)
+
+    def test_case_4(self):
+        product_description = 'GIGABYTE GeForce® GTX  1050TI D5 4G'
+        brand, product_type = BestDeal.extract_product_data(product_description)
+        self.assertEqual('GIGABYTE', brand)
+        self.assertEqual('1050 TI', product_type)
