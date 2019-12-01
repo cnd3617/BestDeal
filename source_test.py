192758ac7ceda489018837debe6844d3db5d71cc
Richard Dally <r.dally@protonmail.com>
2019-12-01 15:59:19 +0100

    Updates unit tests

--- source_test.py
+++ source_test.py
@@ -6,9 +6,8 @@ from topachat import TopAchat
 @pytest.mark.parametrize("fetcher_class", [NVidiaFetcher])
 @pytest.mark.parametrize("source_class", [TopAchat])
 def test_source(fetcher_class, source_class):
-    fetcher = fetcher_class(':in_memory:')
-    mapping = fetcher.get_source_product_urls()[source_class]
-
-    source = source_class()
-    deals = source.fetch_deals(mapping)
-    assert len(deals)
+    """
+    Smoke test
+    """
+    fetcher = fetcher_class(database=None)
+    fetcher._scrap_and_store()
